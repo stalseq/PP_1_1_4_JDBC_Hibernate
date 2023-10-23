e25a1b2854fdb4dde55e21360177dc44f13b1d21
stalseq <136884591+stalseq@users.noreply.github.com>
2023-10-23 14:56:31 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,14 +8,14 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
-        userDao.saveUser("Dwayne", "Johnson", (byte) 51);
-        userDao.saveUser("Vin", "Diesel", (byte) 55);
-        userDao.saveUser("Jason", "Statham", (byte) 56);
-        userDao.saveUser("Bruce", "Willis", (byte) 69);
-        System.out.println(userDao.getAllUsers());
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Dwayne", "Johnson", (byte) 51);
+        userService.saveUser("Vin", "Diesel", (byte) 55);
+        userService.saveUser("Jason", "Statham", (byte) 56);
+        userService.saveUser("Bruce", "Willis", (byte) 69);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
