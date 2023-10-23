cab5513a4cd78a0991f062407afe060be8284a79
stalseq <136884591+stalseq@users.noreply.github.com>
2023-10-23 15:53:16 +0300

    Update UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -5,34 +5,29 @@ import jm.task.core.jdbc.util.Util;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
-public class UserDaoJDBCImpl extends Util implements UserDao {
+public class UserDaoJDBCImpl implements UserDao {
 
-
-    public UserDaoJDBCImpl() throws SQLException {
+    public UserDaoJDBCImpl() {
     }
-    public void createUsersTable() throws SQLException {
-        String sql = "CREATE TABLE IF NOT EXISTS USERS(ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, NAME VARCHAR(45), LASTNAME VARCHAR(45), AGE INT)";
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    public void createUsersTable() {
+        try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS users" +
+                "(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(45), lastname VARCHAR(45), age INT)")) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Во время создания таблицы возникло исключение: " + e);
         }
     }
-    public void dropUsersTable() throws SQLException {
-        String sql = "DROP TABLE IF EXISTS USERS";
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    public void dropUsersTable() {
+        try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("DROP TABLE IF EXISTS users")) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Во время создания таблицы возникло исключение: " + e);
         }
     }
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
-        String sql = "INSERT INTO users(NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    public void saveUser(String name, String lastName, byte age) {
+        try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(name, lastname, age) VALUES(?, ?, ?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setLong(3, age);
@@ -42,19 +37,17 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
             System.err.println("Во время операции возникло исключение: " + e);
         }
     }
-    public void removeUserById(long id) throws SQLException {
-        String sql = "DELETE FROM USERS WHERE ID=?";
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    public void removeUserById(long id) {
+        try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM USERS WHERE id=?")) {
             preparedStatement.setLong(1, id);
         } catch (SQLException e) {
             System.out.println("Во время удаление по id возникло исключение: " + e);
         }
     }
-    public List<User> getAllUsers() throws SQLException {
+    public List<User> getAllUsers() {
         List<User> listUser = new ArrayList<>();
-        String sql = "SELECT ID, NAME, LASTNAME, AGE FROM USERS";
-        try (Connection connection = getConnection(); Statement statement= connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
+        try (Connection connection = Util.getConnection(); Statement statement= connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT id, name, lastname, age FROM users");
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("ID"));
@@ -68,9 +61,8 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
         return listUser;
     }
-    public void cleanUsersTable() throws SQLException {
-        String sql = "DELETE FROM USERS";
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    public void cleanUsersTable() {
+        try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users")) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Во время создания таблицы возникло исключение: " + e);
